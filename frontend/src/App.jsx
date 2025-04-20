acea1603d6131840ca16a00cbacbf37877c5072d
Zhibo Li <z5500393@ad.unsw.edu.au>
2025-04-20 18:08:32 +1000

    Configured the front-end routing system of the entire application, managed the user login status, and rendered corresponding page components under different paths such as login, registration, Dashboard, game editing and title details.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,10 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Navbar from './components/Navbar';
 import MainPage from './components/MainPage';
+import Dashboard from './components/Dashboard';
+import EditGame from './components/EditGame';
+import EditQuestion from './components/EditQuestion';
+import QuestionView from './components/QuestionView';
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
@@ -18,6 +22,10 @@ function App() {
         <Route path='/' element={<MainPage token={token}/>} />
         <Route path='/login' element={<SignIn setToken={setToken} />} />
         <Route path='/register' element={<SignUp setToken={setToken} />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path="/game/:gameId" element={<EditGame />} />
+        <Route path="/game/:gameId/question/:questionId" element={<EditQuestion />} />
+        <Route path="/game/:gameId/question/:questionId/view" element={<QuestionView />} />
       </Routes>
     </BrowserRouter>
   );
